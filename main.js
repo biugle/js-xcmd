4f6e47b29441f3f43b268b7e202dfe8653f8b64c
bex <admin@biugle.cn>
2025-05-14 18:27:28 +0800

    Docs: 1.5.17 Version

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
  * @Author: HxB
  * @Date: 2022-04-28 13:54:21
  * @LastEditors: DoubleAm
- * @LastEditTime: 2024-06-26 09:48:50
+ * @LastEditTime: 2025-05-14 18:25:23
  * @Description: 导出一些方法，或许后面可以用到。
- * @FilePath: \js-xcmd\main.js
+ * @FilePath: /js-xcmd/main.js
  */
 
 const {
@@ -32,7 +32,11 @@ const {
   getAllFilePath
 } = require('./utils/files');
 const { cmd } = require('./utils/cmd');
-const { node2es6, sortJSON, mergeObj, versionUpgrade } = require('./utils/tools');
+const { node2es6, sortJSON, mergeObj, versionUpgrade, isValidJson, jsonToExcel } = require('./utils/tools');
+const { extractParamsFromFiles, filterGitFiles, parseGitFile, getChangedLines } = require('./utils/ast');
+const { FILTER_KEYS } = require('./utils/data');
+const { renderTemplate } = require('./utils/template');
+const { downloadTpl } = require('./utils/tpl');
 
 module.exports = {
   rmRf,
@@ -61,5 +65,14 @@ module.exports = {
   node2es6,
   sortJSON,
   mergeObj,
-  versionUpgrade
+  versionUpgrade,
+  isValidJson,
+  jsonToExcel,
+  extractParamsFromFiles,
+  filterGitFiles,
+  parseGitFile,
+  getChangedLines,
+  FILTER_KEYS,
+  renderTemplate,
+  downloadTpl
 };
